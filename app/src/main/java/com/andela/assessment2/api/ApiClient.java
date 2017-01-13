9013a8049c66e74af4036ab45a907dbf6555eeae
andela-ekarumbi <eston.karumbi@andela.com>
2017-01-13 11:50:57 +0300

    Define API client class

---
+++ app/src/main/java/com/andela/assessment2/api/ApiClient.java
@@ -0,0 +1,22 @@
+package com.andela.assessment2.api;
+
+import com.andela.assessment2.model.BookListResult;
+
+import retrofit2.Callback;
+
+/**
+ * Created by Eston on 13/01/2017.
+ */
+
+public class ApiClient {
+
+    public static String API_BASE;
+
+    public ApiClient() {
+
+    }
+
+    public void getBestSellerHistoryList(Callback<BookListResult> callback) {
+        
+    }
+}
